32874f57d9eb478a5a8198b2aa5c99055efe7133
Tuomas Kinnunen <tuomas.kinnunen@aalto.fi>
2016-01-30 17:59:42 +0200

    added some calls to PoseCheck

--- cult-simulator-2016/Assets/Scripts/SimpleRandom.cs
+++ cult-simulator-2016/Assets/Scripts/SimpleRandom.cs
@@ -1,42 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Handles Cult random Animations and calls PoseCheck
 public class SimpleRandom : StateMachineBehaviour {
-	System.Random randomGen = new System.Random();
+    static private int Bow           = Animator.StringToHash("Base Layer.metarig|bow");
+    static private int LookUp        = Animator.StringToHash("Base Layer.metarig|lookUp");
+    static private int Idle          = Animator.StringToHash("Base Layer.metarig|Idle");
+    static private int HandsForward  = Animator.StringToHash("Base Layer.metarig|HandForward");
+    static private int HandsSideways = Animator.StringToHash("Base Layer.metarig|handsSideways");
+
+    // Animator.StringtoHash not marked as const?!
+    // public enum AnimationNameHashes {
+    // }
+
+    System.Random randomGen = new System.Random();
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
 
-	// OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
-	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
 
-	// OnStateExit is called before OnStateExit is called on any state inside this state machine
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+    // OnStateExit is called before OnStateExit is called on any state inside this state machine
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        float newSpeed = animator.speed;
 
-	// OnStateMove is called before OnStateMove is called on any state inside this state machine
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+        // hashes not constant, cannot use switch case
+        if (stateInfo.fullPathHash == Bow) {
+            newSpeed = GameManager.gm.PoseCheck(Position.none, Position.none, Position.front);
 
-	// OnStateIK is called before OnStateIK is called on any state inside this state machine
-	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+        } else if (stateInfo.fullPathHash == LookUp) {
+            // FIXME: which position?
+            newSpeed = GameManager.gm.PoseCheck(Position.none, Position.none, Position.none);
 
-	// OnStateMachineEnter is called when entering a statemachine via its Entry Node
-	override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash){
-		animator.SetInteger("Selector", randomGen.Next(5));
+        // } else if (stateInfo.fullPathHash == Idle) {
+        //    newSpeed = GameManager.gm.PoseCheck(Position.none, Position.none, Position.none);
+
+        } else if (stateInfo.fullPathHash == HandsForward) {
+            newSpeed = GameManager.gm.PoseCheck(Position.front, Position.front, Position.none);
+
+        } else if (stateInfo.fullPathHash == HandsSideways) {
+            newSpeed = GameManager.gm.PoseCheck(Position.left, Position.right, Position.none);
+
+        } else {
+            Debug.Log("Animation hash not found! not added or typoed!");
+        }
 
 
-	}
+        animator.speed = newSpeed;
+    }
 
-	// OnStateMachineExit is called when exiting a statemachine via its Exit Node
-	//override public void OnStateMachineExit(Animator animator, int stateMachinePathHash) {
-	//
-	//}
+    // OnStateMove is called before OnStateMove is called on any state inside this state machine
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called before OnStateIK is called on any state inside this state machine
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMachineEnter is called when entering a statemachine via its Entry Node
+    override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash){
+        animator.SetInteger("Selector", randomGen.Next(5));
+    }
+
+    // OnStateMachineExit is called when exiting a statemachine via its Exit Node
+    //override public void OnStateMachineExit(Animator animator, int stateMachinePathHash) {
+    //
+    //}
 }
