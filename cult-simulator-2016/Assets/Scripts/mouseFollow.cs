91e7f5fc81aa55aa912af7995d3da036bd886963
Nicholas Saarela <nicholas.saarela@gmail.com>
2016-01-30 16:38:27 +0200

    my very own...

---
+++ cult-simulator-2016/Assets/Scripts/mouseFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseFollow : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 position = Input.mousePosition;
+		position.z = Camera.main.ScreenToWorldPoint (position).z + 1.0f;
+		this.transform.position = Camera.main.ScreenToWorldPoint (position);
+	}
+}
---
+++ cult-simulator-2016/Assets/Scripts/rotate2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate2 : MonoBehaviour {
+	public float speed = 1.0f;
+	public float maxHoriz;
+	public float minHoriz;
+	public float maxVerti;
+	public float minVerti;
+	private float horiz;
+	private float verti;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horiz = speed * Input.GetAxis ("Horizontal2") * Time.deltaTime;
+		Debug.Log(this.transform.eulerAngles);
+		if ((this.transform.eulerAngles.y + horiz > maxHoriz && this.transform.eulerAngles.y + horiz < minHoriz)) {
+			horiz = 0.0f;
+		}
+		verti = speed * Input.GetAxis ("Vertical2") * Time.deltaTime;
+		if ((this.transform.eulerAngles.x + verti > maxVerti && this.transform.eulerAngles.x + verti < minVerti)) {
+			verti = 0.0f;
+		}
+		transform.eulerAngles = new Vector3(this.transform.eulerAngles.x + verti, this.transform.eulerAngles.y + horiz, 0f);
+	}
+}
